a15ca75900139c0160a2076580d6e1e7a72377a8
niveil <n.veil@live.com>
2021-09-29 09:38:33 -0500

    added about section and clean up

--- docs/assets/js/skills.js
+++ docs/assets/js/skills.js
@@ -1,5 +1,6 @@
-var skills = ['#VBA','#MiniTab','#Tableau','#PowerBI','#Laravel','#Drupal','#HTML','#CSS','#JS',
-'#PHP','#SQL','#Python','#SharePoint','#Vue','#Wordpress','#Twig'];
+var skills = ['#VBA','#MiniTab','#Tableau','#PowerBI','#Laravel','#Drupal',
+              '#HTML','#CSS','#JS','#PHP','#SQL','#Python','#SharePoint',
+              '#Vue','#Wordpress','#Twig'];
 
 for (let index = 0; index < skills.length; index++) {
 
@@ -18,22 +19,4 @@ for (let index = 0; index < skills.length; index++) {
     });
 
 clientWindow.observe(document.querySelector('.skills-wrapper'));
-
 }
-// console.log(clientWindow);
-// clientWindow.observe(document.querySelector('#VBA'));
-// clientWindow.observe(document.querySelector('#MiniTab'));
-// clientWindow.observe(document.querySelector('#Tableau'));
-// clientWindow.observe(document.querySelector('#PowerBI'));
-// clientWindow.observe(document.querySelector('#Laravel'));
-// clientWindow.observe(document.querySelector('#Drupal'));
-// clientWindow.observe(document.querySelector('#HTML'));
-// clientWindow.observe(document.querySelector('#CSS'));
-// clientWindow.observe(document.querySelector('#JS'));
-// clientWindow.observe(document.querySelector('#PHP'));
-// clientWindow.observe(document.querySelector('#SQL'));
-// clientWindow.observe(document.querySelector('#Python'));
-// clientWindow.observe(document.querySelector('#SharePoint'));
-// clientWindow.observe(document.querySelector('#Vue'));
-// clientWindow.observe(document.querySelector('#Wordpress'));
-// clientWindow.observe(document.querySelector('#Twig'));
